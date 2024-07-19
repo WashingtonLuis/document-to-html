20933e335a217f6ab3a7c18c664a6cddfccc2031
Washington Luis <washingtonluis@secnec.local>
2024-07-19 09:21:55 -0300

    at 19-07

--- script.js
+++ script.js
@@ -479,8 +479,8 @@ function clear() {
 				.replace(/<ol><li><b>(.*?)<\/b><\/li><\/ol>/gi, "<h5>$1</h5>")
 				.replace(/<p><br><b>0(\d)\)/gi, "<p><br><b>$1)")
 				.replace(/(?<=<p><br><b>\d+\)<\/b>) Resolução:?/gi, "")
-				.replace(/\n?<p><br><\/p> ?\n?(?=<p><b>\((?:EM\d{2}[A-Z]{3}\d{3}|EM[A-Z]{4}\d{2}|EM[A-Z]{6}\d{2})\))/g, "")
-				.replace(/<ol> ?\n?<li>(Trilha de aprendizagem|Objetivo de aprendizagem do capítulo|Situação-problema|Habilidades utilizadas nessa situação-problema:|Resolvendo a situação-problema)<\/li> ?\n?<\/ol>/g, "<p><b>$1</b></p>");
+				.replace(/\s*(?:<p><br><\/p>|<br>)\s*(?=<p><b>\((?:EM\d{2}[A-Z]{3}\d{3}|EM[A-Z]{4}\d{2}|EM[A-Z]{6}\d{2})\))/g, "")
+				.replace(/<ol>\s*<li>(Trilha de aprendizagem|Objetivo de aprendizagem do capítulo|Situação-problema|Habilidades utilizadas nessa situação-problema:|Resolvendo a situação-problema)<\/li>\s*<\/ol>/g, "<p><b>$1</b></p>");
 		}
 
 		textareaValue = textareaValue
