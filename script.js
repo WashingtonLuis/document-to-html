f2ac760a730c3c53a41277cf31e965ed75ae2e44
washington <washingtonldsantos@gmail.com>
2024-12-16 09:34:34 -0300

    16-12

--- script.js
+++ script.js
@@ -320,7 +320,7 @@ function circuit(text) {
 		.replace(/[ ]+/gi, " ")
 		.replace(/(?<=(?:R|Rua|Av\.|Avenida).*?,)\n(\d+)/gi, " $1")
 		.replace(/m ?i ?n/gi, "min")
-		.replace(/(\d+) (.*?), (?:número )?(\d+)(?: |, )?((?:ap|apto|apartamento|bl|b|bloco|t|torre) ?(?:\d+)? ?(?:(?:ap|apto|apartamento|bl|b|bloco|t|torre) ?(?:\d+)?)?)? ?,? (.*?), ?(?:Uberaba,)? ?(\d{5}-\d{3}) (\d+:\d+) ?(\d+:\d+)? ?(?:\((?:Adiantado|Atrasado) .*?\))?(.*?)?/gi, "$1\t$2\t$3\t$5\t$6\t$7\t$8\t$4 $9")
+		.replace(/(\d+) (.*?), (?:número )?(\d+)(?: |, )?((?:ap|apto|apartamento|bl|b|bloco|t|torre|casa|sala) ?(?:\d+)? ?(?:(?:ap|apto|apartamento|bl|b|bloco|t|torre|casa|sala) ?(?:\d+)?)?)? ?,? (.*?), ?(?:Uberaba,)? ?(\d{5}-\d{3}) (\d+:\d+) ?(\d+:\d+)? ?(?:\((?:Adiantado|Atrasado) .*?\))?(.*?)?/gi, "$1\t$2\t$3\t$5\t$6\t$7\t$8\t$4 $9")
 		.replace(/\t[ ]+/gi, "\t")
 		.replace(/apto\b|apartamento|apt\b|AP\b/gi, "ap")
 		.replace(/bloco|bl\b/gi, "b")
