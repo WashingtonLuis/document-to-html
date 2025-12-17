fd2c8e88b1537ca87882f099ad95dc81b8ee355a
washington <washingtonldsantos@gmail.com>
2025-12-17 15:17:18 -0300

    at 17-12

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { alteraElementos } from "./alteraElementos.js";
+import { circuit } from "./circuit.js";
 
 const latexAcentuacao = {
 	á: "\\'a",
@@ -351,105 +352,6 @@ function alternativasAlexandria(text) {
 	return text;
 }
 
-function circuit(text) {
-	text = padronizaCircuit(text);
-	text = corrigirPalavras(text);
-	text = padronizaCircuit(text);
-	return text;
-}
-
-function padronizaCircuit(text) {
-	text = text
-		.replace(/<p># Address<\/p>\n?<p>Estimated Arrival<\/p>\n?<p>Time<\/p>\n?<p>Actual Arrival Time Notes<\/p>\n?/gi, "")
-		.replace(/<p># Address<\/p>\n?<p>Estimated<\/p>\n?<p>Arrival Time<\/p>\n?<p>Notes<\/p>\n?/gi, "")
-		.replace(/(?<=<p>\d+)<\/p>\n<p>(?=Rua|Avenida)/gi, "\t")
-		.replace(/<\/p>\n?<p>((?:\d+)? ?min)/gi, "$1")
-		.replace(/<\/p>\n?<p>((?:\d+)? ?h ?\d+ ?min)/gi, "$1")
-		.replace(/(\d+-)<\/p>\n<p>(\d+)/gi, "$1$2")
-		.replace(/(\d+)<\/p>\n<p>(-\d+)/gi, "$1$2")
-		.replace(/<p>/gi, "")
-		.replace(/<\/p>/gi, "\n")
-		.replace(/\# Address Estimated Arrival Time Actual Arrival Time Notes/gi, "")
-		.replace(/\n\n/g, "\n")
-		.replace(/\n(\d\d:\d\d)/gi, " $1")
-		.replace(/\n([A-Z]+)/gi, " $1")
-		.replace(/\n(\d{1,4}-\d{3})/gi, "$1")
-		.replace(/(\d{1,5}-\d{0,2})\n(\d{1,3})/gi, "$1$2")
-		.replace(/(\d\d:\d\d)/gi, " $1 ")
-		.replace(/[ ]+/gi, " ")
-		.replace(/(?<=(?:R|Rua|Av\.|Avenida).*?,)\n(\d+)/gi, " $1")
-		.replace(/(\d+) (.*?), (?:número )?(\d+)(?: |, )?((?:ap|apto|apartamento|bl|b|bloco|t|torre|casa|sala) ?(?:\d+)? ?(?:(?:ap|apto|apartamento|bl|b|bloco|t|torre|casa|sala) ?(?:\d+)?)?)? ?,? (.*?), ?(?:Uberaba,)? ?(\d{5}-\d{3}) (\d+:\d+) ?(\d+:\d+)? ?(?:\((?:Adiantado|Atrasado) .*?\))?(.*?)?/gi, "$1\t$2\t$3\t$5\t$6\t$7\t$8\t$4 $9")
-		.replace(/\t[ ]+/gi, "\t")
-		.replace(/apto\b|apartamento|apt\b|AP\b/gi, "ap")
-		.replace(/bloco|bl\b/gi, "b")
-		.replace(/torre|T\b/gi, "t")
-		.replace(/[\t ]*(\d{2}:\d{2})[\t ]*/g, "\t$1\t")
-		.replace(/, Uberaba, /gi, "\t")
-		.replace(/\tUberaba\t/gi, "\t")
-		.replace(/(?<=\d+) (?=Rua|Avenida)/gi, "\t")
-		.replace(/(?<=(?:Avenida|Rua)[^,]*, ?\d+), ?/gi, "\t")
-		.replace(/(?<=(?:Avenida|Rua)[^,]*), ??/gi, "\t")
-		.replace(/(?<=\n\d+) /gi, "\t")
-		.replace(/(?<=\n\d+) /gi, "\t")
-		.replace(/(?<=\d+:\d+)\t\t ?(?=\d+:\d+)/gi, "\t")
-		.replace(/\t?\((?:Adiantado|Atrasado).*?\)/gi, "")
-		.replace(/(380\d{0,2}-?)\n(\d|-)/gi, "$1$2")
-		.replace(/<div><br><\/div>/gi, "")
-		.replace(/^\n1/gi, "1")
-		;
-	return text;
-}
-
-	function corrigirPalavras(texto) {
-  const regras = [
-    { correto: "mecânica", base: "mecânica" },
-    { correto: "Uberaba", base: "Uberaba" },
-    { correto: "Cartafina", base: "Cartafina" },
-    { correto: "rota", base: "rota" },
-    { correto: "Costa", base: "Costa" },
-    { correto: "min", base: "min" },
-    { correto: "Prata", base: "Prata" },
-    { correto: "Oliveira", base: "Oliveira" },
-    { correto: "Oliveira Prata", base: "Oliveira Prata" },
-    { correto: "morador", base: "morador" },
-    { correto: "Abadia", base: "Abadia" },
-    { correto: "Silva", base: "Silva" },
-    { correto: "Adiantado", base: "Adiantado" },
-    { correto: "Atrasado", base: "Atrasado" },
-    { correto: "Umuarama", base: "Umuarama" },
-    { correto: "Palmeiras", base: "Palmeiras" },
-    { correto: "tutunas", base: "tutunas" },
-    { correto: "Freire", base: "Freire" },
-    { correto: "Sol", base: "Sol" },
-    { correto: "Canadá", base: "Canadá" },
-    { correto: "Residencial", base: "Residencial" },
-    { correto: "Alfredo", base: "Alfredo" },
-    { correto: "Gerais", base: "Gerais" },
-    { correto: "Borges", base: "Borges" },
-    { correto: "Beija-Flor", base: "Beija-Flor" },
-    { correto: "Conjunto", base: "Conjunto" },
-    { correto: "minas", base: "minas" },
-    { correto: "Morumbi", base: "Morumbi" },
-    { correto: "Professor", base: "Professor" },
-    { correto: "Pacaembu", base: "Pacaembu" },
-    { correto: "bloco", base: "boco" },
-		{ correto: "ramid", base: "ramid" },
-		{ correto: "Mauad", base: "Mauad" },
-		{ correto: "espanha", base: "espanha" },
-		{ correto: "Derenusson", base: "Derenusson" },
-  ];
-
-  for (const { correto, base } of regras) {
-    // Monta regex tipo m\s*e\s*c\s*â\s*n\s*i\s*c\s*a
-    const regex = new RegExp(
-      base.split("").join("\\s*"),
-      "gi"
-    );
-    texto = texto.replace(regex, correto);
-  }
-
-  return texto;
-}
 
 
 function nLatex(str) {
